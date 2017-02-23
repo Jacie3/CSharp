3eff7be5cdbd0ea4529f95684e4ef37d4fe8bf6f
Jacie Jermier <jaciejermier@gmail.com>
2017-02-23 10:46:48 -0800

    Reverse-Number
    Given a number, reverse all of it's digits.

---
+++ ReverseProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num, reverse = 0;
+            Console.WriteLine("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + num % 10;
+                num = num / 10; 
+            }
+            Console.WriteLine("The reverse of the entered number is: " +reverse);
+            Console.ReadLine();
+        }
+    }
+}
